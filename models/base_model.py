1b59484959d2b2dbf795e1172fec08c02cff239e
madokscharles <charlesmadoks@gmail.com>
2023-02-11 14:40:26 -0800

    Updates on BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,48 +1,56 @@
 #!/usr/bin/python3
-""" A class BaseModel that defines all common attributes/methods for other classes """
-
+""" A class BaseModel that defines all common attributes/methods """
 
 import uuid
-from datetime import datetime
+from datetime import datetime as dt
+from models import storage
 
 
 
 class BaseModel:
-    """ class BaseModel: Parent class which other classes inherit from """
+
+    """ class BaseModel: Parent class other class inherit from """
+
     def __init__(self, *args, **kwargs):
-        """ Initializes instance of class BaseModel 
-
-        Using *args and **kwargs arguments for the constructor
-
-        Args: wont be used
-        Kwargs: if kwargs is not empty
+        """ Initializes new instances of BaseModel calss using:
+        
+            Args: Arguments and 
+            Kwargs: Keyword arguments
         """
 
-        if kwargs and kwargs != []:
-            value = kwargs["created_at"]
-            self.id = kwargs["id"]
-            self.created_at = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-            self.updated_at = self.created_at
+        if kwargs and kwargs != {}:
+            for i in kwargs:
+                if i == "created_at":
+                    self.__dict__["created_at"] = dt.strptime(
+                            kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif i == "updated_at":
+                    self.__dict__["updated_at"] = dt.strptime(
+                            kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[i] = kwargs[i]
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = dt.now()
+            self.updated_at = dt.now()
+            storage.new(self)
 
     def __str__(self):
-        """ Prints string representing the class """
+        """ Prints string representation """
 
-        return ("[{}] ({}) {}" .format(type(self).__name__, self.id, self.__dict__))
+        return ("[{}] ({}) {}".format(
+            type(self).__name__, self.id, self.__dict__))
 
     def save(self):
-        """ Updates the public instance attribute updated_at with current datetime """
+        """ Updates the public instance attribute update_at with current time """
 
-        self.updated_at = datetime.now()
+        self.updated_at = dt.now
+        storage.save()
 
     def to_dict(self):
-        """ Returns a dictionary containing all keys/values of __dict__ of the instance """
+        """ Updates a dictionary containing all keys/values of __dict__ of the instance """
 
-        __dict__ = dict(self.__dict__)
-        __dict__['__class__'] = type(self).__name__
-        __dict__['created_at'] = self.created_at.isoformat()
-        __dict__['updated_at'] = self.updated_at.isoformat()
-        return __dict__
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = type(self).__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        return my_dicit
--- models/city.py
+++ models/city.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-"""Defines the City Class"""
+""" Defines the City Class, inherits from BaseModel class """
+
 from models.base_models import BaseModel
 
 
 class City(BaseModel):
-    """Represent a city.
-    Attributes:
-        state_id (str): The state id.
-        name (str): The name of the city.
+    """Represents a city
+
+        Attributes:
+            state_id (str): The state id.
+            name (str): The name of the city.
         """
 
         state id = ""
