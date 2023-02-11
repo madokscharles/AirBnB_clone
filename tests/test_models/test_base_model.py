1e70207c624ad117b088846176b9d585deb6fb77
madokscharles <charlesmadoks@gmail.com>
2023-02-11 02:31:15 +0100

    Update tests/

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,6 +3,7 @@
 
 import unittest
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 from datetime import datetime
 import uuid
 import os
@@ -36,3 +37,30 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsInstance(base.updated_at, datetime)
         self.assertTrue(0 <= (dt - base.created_at).total_seconds() < 1)
         self.assertTrue(0 <= (dt - base.updated_at).total_seconds() < 1)
+
+    def test_update_datetime(self):
+        """ Tests for datetime: update datetime """
+
+        base = self.test_class()
+        dt = datetime.now
+        self.assertIsInstance(base.updated_at, datetime)
+        base.updated_at = datetime.now()
+        self.assertNotEqual(dt, base.updated_at)
+
+    def test_to_str(self):
+        """ Tests __str__ """
+
+        base = self.test_class()
+        _pass = '[' + self.test_name + '] ({}) {}'.format(
+                base.id, str(base.__dict__))
+        self.assertEqual(str(base), _pass)
+
+    def test_save_load(self):
+        """ Tests save and reload """
+
+        if os.path.exists('save.json'):
+            os.remove('save.json')
+        _save = FileStorage()
+        _save.reload()
+        _object = self.test_class()
+        self.assertTrue(self.test_name + '.' + _object.id in _save.all())
